748dd82e668127cfdff1d61211c609044ac5daa1
Kai Walter <kai.walter@outlook.de>
2025-08-23 09:52:19 +0200

    switch to PostgreSQL and RabbitMQ for home servier

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -6,4 +6,19 @@ if [ -f .env ]; then
 	set +a
 fi
 
+# Create folders only if they do not exist
+if [ ! -d ".work/voice" ]; then
+	mkdir -p .work/voice
+fi
+
+if [ ! -d ".data/mq" ]; then
+	mkdir -p .data/mq
+fi
+
+if [ ! -d ".data/db" ]; then
+	mkdir -p .data/db
+	# For Postgres, set ownership so the container can write:
+	sudo chown -R 999:999 .data/db
+fi
+
 docker-compose up -d
